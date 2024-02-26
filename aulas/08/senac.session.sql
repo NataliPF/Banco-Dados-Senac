04850161f520e02ecca84aa0328fdb18f7a54c22
Natali <nana_projetti@hotmail.com>
2024-02-26 20:57:27 -0300

    Criacao scrip final para DQL

--- aulas/08/senac.session.sql
+++ aulas/08/senac.session.sql
@@ -60,3 +60,22 @@ INSERT INTO professor_disciplina (Professor_ID, Disciplina_ID) values
 (5,3),
 (2,3), 	
 (1,3);
+
+-- Atualiza o registro na tabela DDL Update
+UPDATE professor 
+SET especializacao = 'Inteligencia Artifical' 
+WHERE id = 5;
+
+select * from disciplina;
+
+UPDATE disciplina
+SET nome = 'Matemática Avançada', horas = 80
+WHERE ID = 1; 
+
+-- Update de toas as horas para 40 onde Horas for maior que 40
+UPDATE disciplina
+SET horas = 40
+WHERE horas > 40;
+
+-- DML Delete
+DELETE FROM disciplina WHERE id = 3;
